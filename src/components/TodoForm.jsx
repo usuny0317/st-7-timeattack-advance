9ab06757c7ff538ecd9ec673f93d561ab49f3328
usuny0317 <usuny0317@naver.com>
2025-03-07 12:57:19 +0900

    useQueryClient가 아닌 QueryClient를 새로 가져와서 안 됐었습니다

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { todoApi } from "../api/todos";
-import { QueryClient, useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-export default function TodoForm({ fetchData }) {
+export default function TodoForm() {
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
 
   // TODO: 필수: useMutation 으로 리팩터링 하세요.
   // TODO: 선택: useMutation 으로 리팩터링 후, useTodoMutation 커스텀훅으로 정리해 보세요.
 
-  const queryClient = new QueryClient();
+  const queryClient = useQueryClient();
 
   //버튼 눌럿을 때 동작할거
   const handleSubmit = (e) => {
